a66701fb3ec70f0087469aaa5f129f9dbf8ca6c0
cywu <black46821379@gmail.com>
2022-12-17 14:26:40 +0800

    修正.gitattributes 敏感資訊隱藏問題

--- apis/mysqlconnect.py
+++ apis/mysqlconnect.py
@@ -3,11 +3,17 @@ import mysql.connector
 from mysql.connector import pooling
 from difflib import *
 from flask_cors import CORS
+
+MySQL_URL = "testdb.ap-northeast-1.rds.amazonaws.com"
+MySQL_USER = "admin"
+MySQL_PWD = "admin"
+MySQL_DATABASE = "test_db"
+
 dbconfig = {
-	"host":"localhost",
-	"user":"root",
-	"password":"123456",
-	"database":"week"
+	"host":MySQL_URL,
+	"user":MySQL_USER,
+	"password":MySQL_PWD ,
+	"database":MySQL_DATABASE
 }
 mydbpool = pooling.MySQLConnectionPool(
 	pool_name = "mypool",
